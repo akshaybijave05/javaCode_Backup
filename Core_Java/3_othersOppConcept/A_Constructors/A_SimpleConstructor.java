fedd13ef9cc6627a6e700d1d2a3362561cdb2bd3
Mr.Akshay_05 <114489007+akshaybijave05@users.noreply.github.com>
2023-10-25 11:38:52 +0530

    Update A_SimpleConstructor.java

--- Core_Java/3_othersOppConcept/A_Constructors/A_SimpleConstructor.java
+++ Core_Java/3_othersOppConcept/A_Constructors/A_SimpleConstructor.java
@@ -1,6 +1,6 @@
 package A_Constructors;
 
-/*consstrustor is a special type of method whose name is same as class name.
+/*constructor is a special type of method whose name is same as class name.
 note-1)the main purpose of constructor is initialize the object.
 2)every java class has a one default constructor.
 3)a constructor is automatically called at the time of object create.
